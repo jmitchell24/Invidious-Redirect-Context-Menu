e5592d0f2a21b6fbef8289d8c3420b14a915031f
james <james@JM-WORKSTATION>
2019-02-04 23:42:49 -0500

    converted tabs to spaces

--- content-script.js
+++ content-script.js
@@ -1,11 +1,11 @@
 browser.runtime.onMessage.addListener((message) => {
-	switch(message.action) {
-		case "action-bad-url":
-			window.alert(message.message);
-			break;
-			
-		case "action-open-url":
-			window.open(message.message, "_self");
-			break;
-	}
+    switch(message.action) {
+        case "action-bad-url":
+            window.alert(message.message);
+            break;
+            
+        case "action-open-url":
+            window.open(message.message, "_self");
+            break;
+    }
 });
